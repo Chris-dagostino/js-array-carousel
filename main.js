6c0fdc9a07362920393068ed24c7acaf8e0caf0e
Chris-dagostino <dagostino.christian94@gmail.com>
2022-01-26 18:18:01 +0100

    progress

--- main.js
+++ main.js
@@ -22,10 +22,22 @@ const texts = [
     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed ea temporibus aliquam quam neque corrupti cum quibusdam alias maiores in voluptate atque, molestiae quod. Nostrum exercitationem debitis dolorum fugiat deserunt.",
 ];
 
+let slides = '';
+
+let active = 0  ;
 
 for (let i = 0; i < items.length; i++) {
-    const img = items[i];
+    slides +=  `<div class="items">
+                    <img src="${items[i]}">
+                </div>`;           
 }
+
+let slider = document.getElementsByClassName('slider') [0]
+slider.innerHTML = slides
+
+itemsRef.innerHTML = item;
+document.getElementsByClassName('item')[active].classList.add('active');
+
 
 
 
