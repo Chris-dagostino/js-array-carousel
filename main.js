67a9c49ec25c672b4de1ebb219942657a140283b
Chris-dagostino <dagostino.christian94@gmail.com>
2022-01-26 17:09:31 +0100

    start

---
+++ main.js
@@ -0,0 +1,31 @@
+const items = [
+    "img/01.jpg",
+    "img/02.jpg",
+    "img/03.jpg",
+    "img/04.jpg",
+    "img/05.jpg",
+];
+
+const titles = [
+    "Svezia",
+    "Svizzera",
+    "Gran Bretagna",
+    "Germania",
+    "Paradise",
+];
+
+const texts = [
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed ea temporibus aliquam quam neque corrupti cum quibusdam alias maiores in voluptate atque, molestiae quod. Nostrum exercitationem debitis dolorum fugiat deserunt.",
+    "Lorem ipsum .",
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed ea temporibus aliquam quam neque corrupti cum quibusdam alias maiores in voluptate atque, molestiae quod.",
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed ea temporibus aliquam quam neque corrupti cum quibusdam alias maiores in voluptate atque, molestiae quod. Nostrum exercitationem debitis dolorum fugiat deserunt.",
+];
+
+
+for (let i = 0; i < items.length; i++) {
+    const img = items[i];
+}
+
+
+
